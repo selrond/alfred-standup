1f602d7598cf237e8d52223ea3bdbd0d7723bea7
Sebastian Andil <sebastian.andil@gmail.com>
2017-09-27 17:52:30 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Alfred Stand Up
+## Usage
+
+In Alfred, type `sit`, <kbd>Enter</kbd>, and your query.
+
+
+## License
+
+MIT © [Sebastian Andil](https://twitter.com/selrond)
---
+++ index.js
@@ -0,0 +1,32 @@
+"use strict";
+const alfy = require("alfy");
+const notifier = require('node-notifier');
+
+var sittingTimer;
+
+function startSittingTimer() {
+	sittingTimer = setInterval(function() {
+		notifier.notify({
+			'title': '5 seconds have passed!',
+			'message': 'Stand up, take a walk, stretch a bit and come back to work.'
+		})
+	}, 5000);
+}
+
+function stopSittingTimer() {
+	sittingTimer = clearInterval(sittingTimer);
+}
+
+if (alfy.input == "sit") {
+	startSittingTimer();
+	notifier.notify({
+		'title': 'Start working, I\'ll remind you',
+		'message': 'Really...'
+	});
+}	else if (alfy.input == "stand") {
+	stopSittingTimer();
+	notifier.notify({
+		'title': 'Call me when you need me',
+		'message': 'Please do...'
+	});
+}
